ef7bd260b28ba964a4b7b69241ae862c3a7692de
nexovec <hraccesky@gmail.com>
2023-06-29 16:34:46 +0200

    hiding the footer watermark #1

--- webui/static/js/onload.js
+++ webui/static/js/onload.js
@@ -1,4 +1,15 @@
 console.log("onload.js fetched");
-window.addEventListener('DOMContentLoaded', function () {
+// window.addEventListener('DOMContentLoaded', function () {
+window.onload = function () {
     console.log("onload.js loaded");
-});
+    // setTimeout(function () {
+        var footer = document.querySelector('footer');
+    
+        if (footer) {
+            footer.style.display = 'none';
+        } else {
+            console.log("footer not found");
+        }
+    // }, 1000);
+};
+// );
